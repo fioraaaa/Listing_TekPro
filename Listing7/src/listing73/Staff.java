4a805a0ee4c8151483f1056c584db63a6e6d017f
fiora <78577110+fioraaaa@users.noreply.github.com>
2021-05-01 19:11:28 +0700

    minggu 7

--- Listing7/src/listing73/Staff.java
+++ Listing7/src/listing73/Staff.java
@@ -20,8 +20,8 @@ public class Staff {
 		staffList[3] = new Hourly ("Diane", "678 Fifth Ave.", "555-0690", "958-47-3625", 10.55);
 		staffList[4] = new Volunteer ("Norm", "987 Suds Blvd.", "555-8374") ; 
 		staffList[5] = new Volunteer ("Cliff", "321 Duds Lane", "555-7282");
-		staffList[6] = new Commission ("Gazza", "118 Stat Cave", "123-4564", "438-46-6625", 6.25, .20);
-		staffList[7] = new Commission ("Ann", "286 Off Lane", "512-9465", "786-67-3666", 9.75, .15);
+		staffList[6] = new Commission ("Gazza", "118 Stat Cave", "123-4564", "438-46-6625", 6.25, 0.2);
+		staffList[7] = new Commission ("Ann", "286 Off Lane", "512-9465", "786-67-3666", 9.75, 0.15);
 		((Executive)staffList[0]).awardBonus (500.00); 
 		((Hourly)staffList[3]).addHours (40);
 		((Commission)staffList[6]).addHours (35);
